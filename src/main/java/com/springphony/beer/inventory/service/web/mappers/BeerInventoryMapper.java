0bc565eb4f4289569eeb8d69c08bf76516046223
Swapnil <swapnil.khante>
2020-06-26 20:17:02 +0530

    Initial commit with port mapping to 8082

---
+++ src/main/java/com/springphony/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,13 @@
+package com.springphony.beer.inventory.service.web.mappers;
+
+import com.springphony.beer.inventory.service.domain.BeerInventory;
+import com.springphony.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
---
+++ src/test/java/com/springphony/beer/inventory/service/BeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springphony.beer.inventory.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
